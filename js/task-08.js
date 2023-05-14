ebb2a788a5aeb916f64e115994b4a1fd51824e93
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-05-14 20:54:04 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", submit);
+function submit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return alert("Не заповнені поля!!!");
+    }
+    console.log(`Ел. пошта: ${email.value}, Пароль: ${password.value}`);
+    event.currentTarget.reset();
+}
 
