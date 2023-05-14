ac5c4b7a75dacc67da45246f65d6beaead55c1af
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-05-14 21:46:52 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const colorBtn = document.querySelector(".change-color");
+let colorValue = document.querySelector(".color");
+let bgBody = document.querySelector("body");
+colorBtn.addEventListener("click", colorChange);
+
+function colorChange(event) {
+
+  bgBody.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
