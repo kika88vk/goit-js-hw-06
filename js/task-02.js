fea1327e28acd82c69498c95533983d92973580b
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-05-16 11:54:21 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,10 +7,14 @@ const ingredients = [
   'Condiments',
 ];
 const list = document.querySelector("#ingredients");
+const listArray = [];
 
 ingredients.forEach((ingredient) => {
   const item = document.createElement("li");
   item.textContent = ingredient;
   item.classList.add('item');
-  list.append(item);
+  listArray.push(item);
+  
 });
+
+list.append(...listArray);
